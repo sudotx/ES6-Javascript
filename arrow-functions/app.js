fc2cb4f7b62b67d97a130666c6d2e4abd7142871
sumGodbro <64108848+agbadilawal@users.noreply.github.com>
2022-06-28 12:10:04 +0100

    use of arrow functions

---
+++ arrow-functions/app.js
@@ -0,0 +1,14 @@
+// // const sayHello = () => console.log("Hello");
+
+// // const sayHello = () => ({ msg: "Hello" });
+// // console.log(sayHello());
+
+// const sayHello = (name, second) => console.log(`Hello ${name} ${second}`);
+
+// sayHello("oc", "tc")
+
+const users = ["Nathan", "John"];
+
+const namelen = users.map((name) => name.length);
+q
+console.log(namelen);
